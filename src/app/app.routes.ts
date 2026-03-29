eabc6ac69a9603144affc81fb047fcccf3bc08e1
Buffden <buffdenslays@gmail.com>
2026-03-29 07:45:03 -0500

    404 page not found implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,13 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    },
+    {
+        path: 'not-found',
+        loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent),
+    },
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
